0e28bc20b81d22c92395afbe83e05dff1c31915e
S-Dhruv <dhruvsshah171@gmail.com>
2025-10-29 01:07:19 +0530

    Changed URL in login

--- frontend/src/store/userStore.jsx
+++ frontend/src/store/userStore.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const BASE = "http://localhost:5000/";
+const BASE = "https://codingassistant.onrender.com/";
 
 export const userStore = create((set) => ({
   user: null,
